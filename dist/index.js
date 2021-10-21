3c7c56c84b0ab1dd5e34321515e6e047d179f68f
hmmhmmhm <hmmhmmhm@naver.com>
2021-10-21 18:02:14 +0900

    feat: Improved path system

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,12 @@ var _a, _b;
 exports.__esModule = true;
 var fs_1 = require("fs");
 var child_process_1 = require("child_process");
-if (!(0, fs_1.existsSync)('./node_modules')) {
-    var child = (0, child_process_1.spawn)('npm', ['install', '--color=always'], { stdio: 'pipe' });
+var path_1 = require("path");
+if (!(0, fs_1.existsSync)((0, path_1.resolve)(process.cwd(), 'node_modules'))) {
+    var child = (0, child_process_1.spawn)('npm', ['install', '--color=always'], {
+        stdio: 'pipe',
+        cwd: process.cwd()
+    });
     (_a = child.stdout) === null || _a === void 0 ? void 0 : _a.pipe(process.stdout);
     (_b = child.stderr) === null || _b === void 0 ? void 0 : _b.pipe(process.stderr);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { existsSync } from 'fs'
 import { spawn } from 'child_process'
+import { resolve } from 'path'
 
-if (!existsSync('./node_modules')) {
-  const child = spawn('npm', ['install', '--color=always'], { stdio: 'pipe' })
+if (!existsSync(resolve(process.cwd(), 'node_modules'))) {
+  const child = spawn('npm', ['install', '--color=always'], {
+    stdio: 'pipe',
+    cwd: process.cwd()
+  })
   child.stdout?.pipe(process.stdout)
   child.stderr?.pipe(process.stderr)
 }
