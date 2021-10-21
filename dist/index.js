acf43305799a48ce183009571517c8003340a8de
hmmhmmhm <hmmhmmhm@naver.com>
2021-10-21 17:30:38 +0900

    feat: Reduced the bundle size

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 var _a, _b;
 exports.__esModule = true;
-var fs_1 = __importDefault(require("fs"));
+var fs_1 = require("fs");
 var child_process_1 = require("child_process");
-if (!fs_1["default"].existsSync('./node_modules')) {
+if (!(0, fs_1.existsSync)('./node_modules')) {
     var child = (0, child_process_1.spawn)('npm', ['install', '--color=always'], { stdio: 'pipe' });
     (_a = child.stdout) === null || _a === void 0 ? void 0 : _a.pipe(process.stdout);
     (_b = child.stderr) === null || _b === void 0 ? void 0 : _b.pipe(process.stderr);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import fs from 'fs'
+import { existsSync } from 'fs'
 import { spawn } from 'child_process'
 
-if (!fs.existsSync('./node_modules')) {
+if (!existsSync('./node_modules')) {
   const child = spawn('npm', ['install', '--color=always'], { stdio: 'pipe' })
   child.stdout?.pipe(process.stdout)
   child.stderr?.pipe(process.stderr)
