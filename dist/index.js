b2ed8c1dd739b183c007a646428690c023427cfd
hmmhmmhm <hmmhmmhm@naver.com>
2021-10-24 15:26:06 +0900

    feat: Update ci and install

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,8 @@ var fs_1 = require("fs");
 var child_process_1 = require("child_process");
 var path_1 = require("path");
 if (!(0, fs_1.existsSync)((0, path_1.resolve)(process.cwd(), 'node_modules'))) {
-    var child = (0, child_process_1.spawn)('npm', ['ci', '--color=always'], {
+    var isPackageLockExist = (0, fs_1.existsSync)((0, path_1.resolve)(process.cwd(), 'package-lock.json'));
+    var child = (0, child_process_1.spawn)('npm', [isPackageLockExist ? 'ci' : 'install', '--color=always'], {
         stdio: 'pipe',
         cwd: process.cwd()
     });
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import { spawn } from 'child_process'
 import { resolve } from 'path'
 
 if (!existsSync(resolve(process.cwd(), 'node_modules'))) {
-  const child = spawn('npm', ['ci', '--color=always'], {
-    stdio: 'pipe',
-    cwd: process.cwd()
-  })
+  const isPackageLockExist = existsSync(
+    resolve(process.cwd(), 'package-lock.json')
+  )
+  const child = spawn(
+    'npm',
+    [isPackageLockExist ? 'ci' : 'install', '--color=always'],
+    {
+      stdio: 'pipe',
+      cwd: process.cwd()
+    }
+  )
   child.stdout?.pipe(process.stdout)
   child.stderr?.pipe(process.stderr)
 }
